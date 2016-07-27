9cb419b70cd6cb50c98b7324d1211abb43b3bc33
Devdatta <devdatta.godbole777@gmail.com>
2016-07-27 20:59:30 +0530

    Adds Final parameterwise fetching

--- DevWebServicePOC/App_Start/WebApiConfig.cs
+++ DevWebServicePOC/App_Start/WebApiConfig.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using System.Net.Http.Headers;
 
 namespace DevWebServicePOC
 {
@@ -10,10 +11,16 @@ namespace DevWebServicePOC
         public static void Register(HttpConfiguration config)
         {
             config.Routes.MapHttpRoute(
+                name: "WithActionApi",
+                routeTemplate: "api/{controller}/{action}/{ProductID}"
+            );
+            config.Routes.MapHttpRoute(
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+            config.Formatters.JsonFormatter.
+            SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
         }
     }
 }
