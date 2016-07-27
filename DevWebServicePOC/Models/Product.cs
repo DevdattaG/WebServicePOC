efb7941c43ce589827059876f3d5d5b148e1efd3
Devdatta <devdatta.godbole777@gmail.com>
2016-07-27 17:46:59 +0530

    Initial Commit

---
+++ DevWebServicePOC/Models/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DevWebServicePOC.Models
+{
+    public class Product
+    {
+        public int ProductID { get; set; }
+        public string ProductName { get; set; }
+        public string ProductCatagory { get; set; }
+        public int ProductPrice { get; set; }
+    }
+}
