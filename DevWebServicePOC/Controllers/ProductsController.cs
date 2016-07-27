70636203d8d19299c428ef72ba38e7e0bf7b7fd8
Devdatta <devdatta.godbole777@gmail.com>
2016-07-27 18:15:54 +0530

    Adds initial ProductController

---
+++ DevWebServicePOC/Controllers/ProductsController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DevWebServicePOC.Models;
+
+namespace DevWebServicePOC.Controllers
+{
+    public class ProductsController : Controller
+    {
+        //
+        // GET: /Products/
+        Product[] Products = new Product[]
+        {
+    new Product { ProductID = 1, ProductName = "Product 1", ProductCatagory= "Category 1", ProductPrice = 120 },
+    new Product { ProductID = 2, ProductName = "Product 2", ProductCatagory= "Category 2", ProductPrice = 200}
+        };
+
+        public IEnumerable<Product> GetProducts()
+        {
+            return Products;
+        }
+        
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
